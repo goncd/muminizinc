cf8bf0bda92043210c221a5a0908cf5f3a900e11
goncd <27208645+goncd@users.noreply.github.com>
2025-09-30 00:27:11 +0200

    Add testing infrastructure
    Boost.Test + CTest seems like a good fit for this project. The tests
    are empty for now, though.

---
+++ tests/test_mutant_calls.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE test_mutant_calls
+
+#include <boost/test/unit_test.hpp>
---
+++ tests/test_mutant_relational.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE test_mutant_relational
+
+#include <boost/test/unit_test.hpp>
